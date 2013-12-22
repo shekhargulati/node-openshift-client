44ecc51792401a3f4171d168ab27242773fb92f1
Shekhar Gulati <shekhargulati84@gmail.com>
2013-12-22 22:31:45 +0530

    added test cases and functions now take result callback

--- test/node-openshift-client_test.js
+++ test/node-openshift-client_test.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var node-openshift-client = require('../lib/node-openshift-client.js');
+var OpenShift = require('../lib/openshift.js');
+
+
+
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -22,15 +25,32 @@ var node-openshift-client = require('../lib/node-openshift-client.js');
     test.ifError(value)
 */
 
-exports.node-openshift-client = {
+exports.openshift = {
   setUp: function(done) {
-    // setup here
+    this.openshift = new OpenShift({username : process.env.OPENSHIFT_USERNAME,password : process.env.OPENSHIFT_PASSWORD});
     done();
   },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(node-openshift-client.awesome(), 'awesome', 'should be awesome.');
-    test.done();
+  'should fetch autorization token': function(test) {
+    
+    test.expect(2);
+    function resultCallback(error , result){
+      // console.log(JSON.parse(result));
+      test.ok(!error , 'there should not be any error');
+      test.ok(result,'should be not null');
+      test.done();
+    }
+    this.openshift.authorizationToken(resultCallback);
+  },
+  'should fetch user details' : function(test){
+    test.expect(4);
+    function resultCallback(error , result){
+      test.ok(!error , 'there should not be any error');
+      var jsonResult = JSON.parse(result);
+      test.equal(jsonResult.data.login,'shekhar.redhat@gmail.com','username should be shekhar.redhat@gmail.com');
+      test.equal(jsonResult.data.plan_id,'free','account is using free plan');
+      test.equal(jsonResult.data.plan_state , 'ACTIVE','account is active');
+      test.done();
+    }
+    this.openshift.showUser(resultCallback);
   }
 };
