1694ac503d60360c9afa403d1e6b91488e0bc1d1
Shekhar Gulati <shekhargulati84@gmail.com>
2013-12-23 01:31:36 +0530

    added test cases for application.. all of these exist in a group to avoid parallel test issues

--- test/node-openshift-client_test.js
+++ test/node-openshift-client_test.js
@@ -25,8 +25,6 @@ var OpenShift = require('../lib/openshift.js');
     test.ifError(value)
 */
 
-function noop() {}
-
 exports['authorization tests'] = {
   setUp: function(done) {
     this.openshift = new OpenShift({username : process.env.OPENSHIFT_USERNAME,password : process.env.OPENSHIFT_PASSWORD});
@@ -73,24 +71,26 @@ exports['user tests'] = {
   }
 };
 
-exports['domain tests'] = {
+exports['domain application tests'] = {
   setUp: function(done){
     this.openshift = new OpenShift({username : process.env.OPENSHIFT_USERNAME,password : process.env.OPENSHIFT_PASSWORD});
-    console.log('\n in setUp()... ');
     done();
   },
   
   'should create domain' : function(test){
-    test.expect(1);
+    console.log('\n Running test \'should create domain\'');
+    test.expect(2);
     function resultCallback(error , result){
       test.ok(!error , 'there should not be any error');
       var jsonResult = JSON.parse(result);
+      test.ok(jsonResult,'result should be truthy');
       test.done();
     }
     this.openshift.createDomain('onopenshiftcloud',resultCallback);
   },
 
-  'should list all domains for a user' : function(test){
+  'should list all domains' : function(test){
+    console.log('\n Running test \'should list all domains\'');
     test.expect(4);
     function resultCallback(error , result){
       test.ok(!error , 'there should not be any error');
@@ -103,6 +103,7 @@ exports['domain tests'] = {
     this.openshift.listDomains(resultCallback);
   },
   'should view domain details' : function(test){
+    console.log('\n Running test \'should view domain details\'');
     test.expect(3);
     function resultCallback(error , result){
       test.ok(!error , 'there should not be any error');
@@ -113,7 +114,43 @@ exports['domain tests'] = {
     }
     this.openshift.viewDomainDetails('onopenshiftcloud',resultCallback);
   },
+
+  'should create application' : function(test){
+    console.log('\n Running test \'should create application\'');
+    test.expect(7);
+    function resultCallback(error , result){
+      var jsonResult = JSON.parse(result);
+      test.ok(!error ,'should not have error message '+error);
+      test.ok(jsonResult , 'result not null');
+      test.equal(jsonResult.data.app_url ,'http://myfirstapp-onopenshiftcloud.rhcloud.com/','app url should be http://myfirstapp-onopenshiftcloud.rhcloud.com/');
+      test.equal(jsonResult.data.name ,'myfirstapp','app name should be myfirstapp');
+      test.equal(jsonResult.data.scalable,true , 'app should be scaleable');
+      test.equal(jsonResult.data.framework,'php-5.3','web cartridge should be php-5.3');
+      test.ok(jsonResult.data.git_url,'git url should be truthy');
+      test.done();
+    }
+    var app_options = {
+      name : 'myfirstapp',
+      cartridge : 'php-5.3',
+      scale : true
+    };
+    this.openshift.createApplication('onopenshiftcloud',app_options, resultCallback);
+  },
+
+  'should delete application' : function(test){
+    console.log('\n Running test \'should delete application\'');
+    test.expect(3);
+    function resultCallback(error , result){
+      test.ok(!error , 'there should not be any error');
+      var jsonResult = JSON.parse(result);
+      test.ok(jsonResult , 'result not null');
+      test.equal(jsonResult.messages[0].text,'Application myfirstapp is deleted.','success message should be not null');
+      test.done();
+    }
+    this.openshift.deleteApplication('onopenshiftcloud','myfirstapp', resultCallback);
+  },
   'should delete domain' : function(test){
+    console.log('\n Running test \'should delete domain\'');
     test.expect(3);
     function resultCallback(error , result){
       test.ok(!error , 'there should not be any error');
